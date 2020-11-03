5702c7271e3746b65b8b915cd12e37bdd8f18e54
gooseisdead <gooseisdead@mac.com>
2020-11-03 17:02:01 -0500

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  const createTaskForm = document.getElementById('create-task-form')
+
+  createTaskForm.addEventListener('submit',(event) => { 
+    event.preventDefault();
+    const userInput = event.target.querySelector('#new-task-description')
+    const ul = document.getElementById('tasks')
+    const li = document.createElement('li')
+      li.innerText = userInput.value
+        ul.appendChild(li)
+
+    const deleteButton = document.createElement('button')
+      deleteButton.innerText = ' ❌ DONE'
+        li.append(deleteButton)
+    deleteButton.addEventListener('click',() => {
+          const result = confirm('Done with this task?')
+          if (result){
+            li.remove()
+          }
+        })
+  })
+
+})
+
